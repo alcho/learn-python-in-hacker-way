54eaf68e3efda44e192b0dcd3838bce76af6fda1
Cho, Yu-Chen <acho@suse.com>
2012-04-18 15:33:12 +0800

    Week 1 assignments

---
+++ courses/novice/exercises/week1/A3.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+題目:
+
+寫一隻程式讓使用者可以輸入字串，顯示其 Unicode，其輸出如下
+
+input:中
+u'\u4e2d'
+
+提示: 可以在print時使用 repr
+"""
+
+print repr(unicode(raw_input("input:"),'utf-8'))
